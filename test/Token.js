cd3e16cefded8c25387f54e9f5cca29707c9cfbb
DavidTaapy <DavidTayAP@Hotmail.Com>
2021-12-12 16:01:32 +0800

    Update Token.js

--- test/Token.js
+++ test/Token.js
@@ -3,10 +3,10 @@ const { expect } = require('chai');
 describe('Token contract', () => {
     let Token, token, owner, addr1, addr2;
 
-    beforeEach (async () => {
+    beforeEach(async () => {
         Token = await ethers.getContractFactory('Token');
         token = await Token.deploy();
-        [owner, addr1, addr2, _] = ethers.getSigners();
+        [owner, addr1, addr2, _] = await ethers.getSigners();
     });
 
     describe('Deployment', () => {
@@ -15,7 +15,7 @@ describe('Token contract', () => {
         });
 
         it('Should assign the total supply of token to the owners', async () => {
-            const ownerBalance = token.balanceOf(owner.address);
+            const ownerBalance = await token.balanceOf(owner.address);
             expect(await token.totalSupply()).to.equal(ownerBalance);
         });
     });
@@ -25,9 +25,32 @@ describe('Token contract', () => {
             await token.transfer(addr1.address, 25);
             const addr1Balance = await token.balanceOf(addr1.address);
             expect(addr1Balance).to.equal(25);
-            await token.transfer(addr2.address, 44);
+            // Transfer to address 2 from address 1 a sum of 16 tokens
+            await token.connect(addr1).transfer(addr2.address, 16);
             const addr2Balance = await token.balanceOf(addr2.address);
-            expect(addr2Balance).to.equal(44);
+            expect(addr2Balance).to.equal(16);
+        });
+
+        it('Should fail if sender does not have tokens', async () => {
+            const initialBalanceOwner = await token.balanceOf(owner.address);
+            await expect(
+                token.connect(addr1).transfer(owner.address, 1)
+            ).to.be.revertedWith('Not enough tokens');
+            expect(
+                await token.balanceOf(owner.address)
+            ).to.equal(initialBalanceOwner);
+        });
+
+        it('Should update post-transfer balances', async () => {
+            const initialBalanceOwner = await token.balanceOf(owner.address);
+            await token.transfer(addr1.address, 9);
+            await token.transfer(addr2.address, 16);
+            const finalOwnerBalance = await token.balanceOf(owner.address);
+            expect(finalOwnerBalance).to.equal(initialBalanceOwner - 25);
+            const addr1Balance = await token.balanceOf(addr1.address);
+            expect(addr1Balance).to.equal(9);
+            const addr2Balance = await token.balanceOf(addr2.address);
+            expect(addr2Balance).to.equal(16);
         });
     });
 });
