7481f1415a1c3104f678a7145982cce18f58f56c
DavidTaapy <DavidTayAP@Hotmail.Com>
2021-12-13 00:15:44 +0800

    Deployment Scripts
    Scripts to deploy application

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,14 @@
  */
 
 require('@nomiclabs/hardhat-waffle');
+const secrets = require('./secrets.json');
 
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: secrets.INFURA_URL,
+      accounts: [`0x${secrets.PRIVATE_KEY}`]
+    }
+  }
 };
